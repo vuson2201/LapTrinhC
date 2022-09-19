ec6340a0cf455f5752536971d3f7477d4b6b8584
vuson2201 <vuson1921@gmail.com>
2022-09-19 11:11:46 +0700

    remo BTCB

--- Bai_tap_co_ban/Bai_tap_co_ban/Bai1.cs
+++
@@ -1,6 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-int n;
-Console.WriteLine("Nhap vao 1 so nguyen:");
-n = int.Parse(Console.ReadLine());
-if (n >= 0) Console.WriteLine("Day la so nguyen duong");
-else Console.WriteLine("Day la so nguyen am");
